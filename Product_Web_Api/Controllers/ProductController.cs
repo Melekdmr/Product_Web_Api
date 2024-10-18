8bf5924adfbee3875c4abcee4dc0b33decafc8c8
Melekdmr <melekdmrr01@gmail.com>
2024-10-18 21:54:16 +0300

    Add project files.

---
+++ Product_Web_Api/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Product_Web_Api.Core;
+
+namespace Product_Web_Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductController : ControllerBase
+	{
+		private static List<ProductEntity> products = new List<ProductEntity>{
+				  new ProductEntity {Id=1,Name="Cep Telefonu" , Price="20000"},
+				  new ProductEntity {Id=2,Name="Bilgisayar" , Price="50000"},
+				  new ProductEntity {Id=3,Name="PowerBank" , Price="1000"},
+
+			};
+[HttpGet]
+		public async  Task< IActionResult> Get()
+		{
+			return Ok(products);
+		}
+		//[HttpGet("{id}")] 
+		//public async Task<IActionResult>Get(int id)
+		
+	}
+}
+
---
+++ Product_Web_Api/Core/ProductEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Product_Web_Api.Core
+{
+	public class ProductEntity
+	{
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Price { get; set; }
+    }
+    
+}
